06bfc3b0b5f5722662428e85276256d2b640996d
Folke Schönberg <folke.schonberg@elev.ga.lbs.se>
2024-12-07 22:50:33 +0100

    Day 7 commit

--- Day6Part2.cpp
+++ Day6Part2.cpp
@@ -1,358 +1,315 @@
-//#include <iostream>
-//#include <vector>
-//#include <fstream>
-//#include <string>
+//16616 down
 //
-//struct int2
-//{
-//	int x;
-//	int y;
-//};
+//3496 down
 //
-//struct int2FS
-//{
-//	int first;
-//	int second;
-//};
+//407 upp
 //
-//struct ObsAria {
-//	int rowFirst;
-//	int rowSecond;
-//	int columnFirst;
-//	int columnSecond;
-//	bool aimingPositiv;
-//};
+//214 nop
 //
-//#define OBST	(char)0b00000001
-//#define UPP		(char)0b00000010
-//#define DOWN	(char)0b00000100
-//#define RIGHT	(char)0b00001000
-//#define LEFT	(char)0b00010000
-//#define BHEAR	(char)0b00100000
+//440 nop
 //
+//421 nop
 //
+//1834 nop
 //
-//bool contains(std::vector<int2>& vec, int2 pos)
-//{
+//1978 nop
 //
-//	for (int i = 0; i < vec.size(); i++)
-//	{
-//		if (vec[i].x == pos.x && vec[i].y == pos.y)
-//		{
-//			return true;
-//		}
-//	}
-//	return false;
-//}
+//1835 nop
 //
-//int main()
-//{
-//
-//	std::ifstream file("Day6Inp.txt");
-//
-//	std::vector<std::string> InpVec;
-//	std::string line;
-//
-//	while (std::getline(file, line))
-//	{
-//		InpVec.push_back(line);
-//	}
-//
-//	int2 gardPosition = { 0,0 };
-//
-//	// find gard
-//	for (int i = 0; i < InpVec.size(); i++)
-//	{
-//		for (int j = 0; j < InpVec[0].size(); j++)
-//		{
-//			if (InpVec[i][j] == '^')
-//			{
-//				gardPosition.y = i;
-//				gardPosition.x = j;
-//				InpVec[i][j] = '.';
-//				goto abcd;
-//			}
-//		}
-//	}
-//abcd:
-//
-//	std::vector<std::vector<uint8_t>> newGrid;
-//
-//	for (int y = 0; y < InpVec.size(); y++)
-//	{
-//		newGrid.emplace_back(std::vector<uint8_t>());
-//		for (int x = 0; x < InpVec[0].size(); x++)
-//		{
-//			if (InpVec[y][x] == '#')
-//			{
-//				newGrid[y].emplace_back((uint8_t)0 | OBST);
-//			}
-//			else
-//			{
-//				newGrid[y].emplace_back((uint8_t)0);
-//			}
-//		}
-//	}
-//
-//	std::string str;
-//
-//	for (int i = 0; i < newGrid.size(); i++)
-//	{
-//		for (int j = 0; j < newGrid[i].size(); j++)
-//		{
-//			if ((newGrid[i][j] & OBST) == OBST)
-//			{
-//				str += "#";
-//			}
-//			else if (newGrid[i][j] != 0)
-//			{
-//				str += "+";
-//			}
-//			else
-//				str += ".";
-//		}
-//		str += "\n";
-//	}
-//
-//	std::vector<int2> ObsticalPositions;
-//
-//	int2 dirVector = { 0, -1 };
-//
-//	int iterations = 0;
-//
-//	while (gardPosition.y < newGrid.size() - 1 && gardPosition.y > 0 && gardPosition.x < newGrid[0].size() - 1 && gardPosition.x > 0)
-//	{
-//
-//		if ((newGrid[gardPosition.y + dirVector.y][gardPosition.x + dirVector.x] & OBST) != 0)
-//		{
-//			if (dirVector.y == -1 && dirVector.x == 0)
-//			{
-//				int tempy = gardPosition.y;
-//				while (tempy < newGrid.size() && (newGrid[tempy][gardPosition.x] & OBST) == 0)
-//				{
-//					if ((newGrid[tempy][gardPosition.x] & UPP) != 0)
-//						throw;
-//					newGrid[tempy][gardPosition.x] = newGrid[tempy][gardPosition.x] | UPP;
-//
-//					tempy++;
-//				}
-//				
-//				dirVector.y = 0;
-//				dirVector.x = 1;
-//			}
-//			else if (dirVector.y == 0 && dirVector.x == 1)
-//			{
-//
-//				int tempx = gardPosition.x;
-//				while (tempx > 0 && (newGrid[gardPosition.y][tempx] & OBST) == 0)
-//				{
-//					if ((newGrid[gardPosition.y][tempx] & RIGHT) != 0)
-//						throw;
-//					newGrid[gardPosition.y][tempx] = newGrid[gardPosition.y][tempx] | RIGHT;
-//
-//					tempx--;
-//				}
-//
-//				dirVector.y = 1;
-//				dirVector.x = 0;
-//			}
-//			else if (dirVector.y == 1 && dirVector.x == 0)
-//			{
-//
-//				int tempy = gardPosition.y;
-//				while (tempy > 0 && (newGrid[tempy][gardPosition.x] & OBST) == 0)
-//				{
-//					if ((newGrid[tempy][gardPosition.x] & DOWN) != 0)
-//						throw;
-//					newGrid[tempy][gardPosition.x] = newGrid[tempy][gardPosition.x] | DOWN;
-//
-//					tempy--;
-//				}
-//
-//				dirVector.y = 0;
-//				dirVector.x = -1;
-//			}
-//			else if (dirVector.y == 0 && dirVector.x == -1)
-//			{
-//
-//				int tempx = gardPosition.x;
-//				while (tempx < newGrid[0].size() && (newGrid[gardPosition.y][tempx] & OBST) == 0)
-//				{
-//					if ((newGrid[gardPosition.y][tempx] & LEFT) != 0)
-//						throw;
-//					newGrid[gardPosition.y][tempx] = newGrid[gardPosition.y][tempx] | LEFT;
-//
-//					tempx++;
-//				}
-//
-//				dirVector.y = -1;
-//				dirVector.x = 0;
-//			}
-//			else
-//				throw;
-//		}
-//		else
-//		{
-//
-//			if ((newGrid[gardPosition.y][gardPosition.x] & BHEAR) == BHEAR && (newGrid[gardPosition.y][gardPosition.x] - BHEAR) == 0)
-//				throw;
-//
-//
-//			if (dirVector.y == -1 && dirVector.x == 0)
-//			{
-//				if ((newGrid[gardPosition.y][gardPosition.x] & RIGHT) == RIGHT) {
-//
-//					if (!contains(ObsticalPositions, { gardPosition.x + dirVector.x, gardPosition.y + dirVector.y }) && newGrid[gardPosition.y + dirVector.y][gardPosition.x + dirVector.x] == 0)
-//					{
-//						ObsticalPositions.emplace_back(gardPosition.x + dirVector.x, gardPosition.y + dirVector.y);
-//					}
-//
-//				}
-//			}
-//			else if (dirVector.y == 0 && dirVector.x == 1)
-//			{
-//				if ((newGrid[gardPosition.y][gardPosition.x] & DOWN) == DOWN) {
-//
-//					if (!contains(ObsticalPositions, { gardPosition.x + dirVector.x, gardPosition.y + dirVector.y }) && newGrid[gardPosition.y + dirVector.y][gardPosition.x + dirVector.x] == 0)
-//					{
-//						ObsticalPositions.emplace_back(gardPosition.x + dirVector.x, gardPosition.y + dirVector.y);
-//					}
-//
-//				}
-//			}
-//			else if (dirVector.y == 1 && dirVector.x == 0)
-//			{
-//				if ((newGrid[gardPosition.y][gardPosition.x] & LEFT) == LEFT) {
-//
-//					if (!contains(ObsticalPositions, { gardPosition.x + dirVector.x, gardPosition.y + dirVector.y }) && newGrid[gardPosition.y + dirVector.y][gardPosition.x + dirVector.x] == 0)
-//					{
-//						ObsticalPositions.emplace_back(gardPosition.x + dirVector.x, gardPosition.y + dirVector.y);
-//					}
-//
-//				}
-//			}
-//			else if (dirVector.y == 0 && dirVector.x == -1)
-//			{
-//				if ((newGrid[gardPosition.y][gardPosition.x] & UPP) == UPP) {
-//
-//					if (!contains(ObsticalPositions, { gardPosition.x + dirVector.x, gardPosition.y + dirVector.y }) && newGrid[gardPosition.y + dirVector.y][gardPosition.x + dirVector.x] == 0)
-//					{
-//						ObsticalPositions.emplace_back(gardPosition.x + dirVector.x, gardPosition.y + dirVector.y);
-//					}
-//
-//				}
-//			}
-//			else
-//				throw;
-//			
-//			newGrid[gardPosition.y][gardPosition.x] += BHEAR;
-//
-//			gardPosition.y += dirVector.y;
-//			gardPosition.x += dirVector.x;
-//
-//			
-//			if (iterations % 1 == 0)
-//			{
-//				str.clear();
-//
-//				for (int i = 0; i < newGrid.size(); i++)
-//				{
-//					for (int j = 0; j < newGrid[i].size(); j++)
-//					{
-//						if ((newGrid[i][j] & OBST) == OBST)
-//						{
-//							str += "#";
-//						}
-//						else if ((newGrid[i][j] & UPP) == UPP)
-//						{
-//							str += "^";
-//						}
-//						else if ((newGrid[i][j] & RIGHT) == RIGHT)
-//						{
-//							str += ">";
-//						}
-//						else if ((newGrid[i][j] & DOWN) == DOWN)
-//						{
-//							str += "!";
-//						}
-//						else if ((newGrid[i][j] & LEFT) == LEFT)
-//						{
-//							str += "<";
-//						}
-//						
-//						else if ((newGrid[i][j] & BHEAR) == BHEAR && (newGrid[i][j] & LEFT) != LEFT && (newGrid[i][j] & DOWN) != DOWN && 
-//							(newGrid[i][j] & RIGHT) != RIGHT && (newGrid[i][j] & UPP) != UPP)
-//						{
-//							str += "X";
-//						}
-//						else
-//							str += ".";
-//					}
-//					str += "\n";
-//				}
-//			}
-//			
-//
-//		}
-//
-//		if (iterations % 5790 == 0)
-//		{
-//			std::cout << "";
-//		}
-//
-//		iterations++;
-//	}
-//
-//	str.clear();
-//
-//	for (int i = 0; i < newGrid.size(); i++)
-//	{
-//		for (int j = 0; j < newGrid[i].size(); j++)
-//		{
-//			if ((newGrid[i][j] & OBST) == OBST)
-//			{
-//				str += "#";
-//			}
-//			else if ((newGrid[i][j] & UPP) == UPP)
-//			{
-//				str += "^";
-//			}
-//			else if ((newGrid[i][j] & RIGHT) == RIGHT)
-//			{
-//				str += ">";
-//			}
-//			else if ((newGrid[i][j] & DOWN) == DOWN)
-//			{
-//				str += "!";
-//			}
-//			else if ((newGrid[i][j] & LEFT) == LEFT)
-//			{
-//				str += "<";
-//			}
-//			else if ((newGrid[i][j] & BHEAR) == BHEAR)
-//			{
-//				str += "X";
-//			}
-//			else
-//				str += ".";
-//		}
-//		str += "\n";
-//	}
-//
-//	std::cout << ObsticalPositions.size();
-//
-//}
-//
-////16616 down
-////
-////3496 down
-////
-////407 upp
-////
-////214 nop
-////
-////440 nop
-////
-////421 nop
-////
-////
+//1784
+
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+#define NUTHING	0b00000001
+#define BLOCK	0b00000010
+
+struct int2
+{
+	int x;
+	int y;
+};
+
+struct int4
+{
+	int posx;
+	int posy;
+	int dirx;
+	int diry;
+};
+
+bool containes(std::vector<int2> vec, int x, int y)
+{
+	for (int i = 0; i < vec.size(); i++)
+	{
+		if (vec[i].x == x && vec[i].y == y)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool containesi4(std::vector<int4> vec, int posx, int posy, int dirx, int diry)
+{
+	for (int i = 0; i < vec.size(); i++)
+	{
+		if (vec[i].posx == posx && vec[i].posy == posy && vec[i].dirx == dirx && vec[i].diry == diry)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int main()
+{
+
+	std::ifstream file("Day6Inp.txt");
+
+	std::vector<std::vector<char>> inpVec;
+	std::string line;
+
+	int2 gardPos = { 0, 0 };
+	int2 gardPosStart = { 0, 0 };
+
+	while (std::getline(file, line))
+	{
+		inpVec.emplace_back(std::vector<char>());
+
+		for (int i = 0; i < line.size(); i++)
+		{
+			if (line[i] == '.')
+			{
+				inpVec[inpVec.size() - 1].emplace_back(NUTHING);
+			}
+			else if (line[i] == '#')
+			{
+				inpVec[inpVec.size() - 1].emplace_back(BLOCK);
+			}
+			else if (line[i] == '^')
+			{
+				inpVec[inpVec.size() - 1].emplace_back(NUTHING);
+				gardPos.x = i;
+				gardPos.y = inpVec.size() - 1;
+				gardPosStart.x = i;
+				gardPosStart.y = inpVec.size() - 1;
+			}
+			else
+				throw;
+		}
+	}
+
+	int2 gardDir = { 0, -1 };
+
+	uint64_t sum = 0;
+
+	for (int y = 0; y < inpVec.size(); y++)
+	{
+		for (int x = 0; x < inpVec[y].size(); x++)
+		{
+
+			int2 testGardDir = gardDir;
+			int2 testGardPos = gardPos;
+
+			std::vector<int4> bineHear;
+
+			std::vector<std::vector<char>> testInpVec(inpVec);
+
+			testInpVec[y][x] = BLOCK;
+
+			bool loopFound = false;
+
+			while (testGardPos.y < testInpVec.size() - 1 && testGardPos.y > 0 && testGardPos.x < testInpVec[0].size() - 1 && testGardPos.x > 0)
+			{
+				if (testInpVec[testGardPos.y + testGardDir.y][testGardPos.x + testGardDir.x] == BLOCK)
+				{
+					if (testGardDir.y == -1 && testGardDir.x == 0)
+					{
+						testGardDir.y = 0;
+						testGardDir.x = 1;
+					}
+					else if (testGardDir.y == 0 && testGardDir.x == 1)
+					{
+						testGardDir.y = 1;
+						testGardDir.x = 0;
+					}
+					else if (testGardDir.y == 1 && testGardDir.x == 0)
+					{
+						testGardDir.y = 0;
+						testGardDir.x = -1;
+					}
+					else if (testGardDir.y == 0 && testGardDir.x == -1)
+					{
+						testGardDir.y = -1;
+						testGardDir.x = 0;
+					}
+					else
+						throw;
+				}
+				else
+				{
+					if (containesi4(bineHear, testGardPos.x, testGardPos.y, testGardDir.x, testGardDir.y))
+					{
+						loopFound = true;
+						break;
+					}
+					else
+					{
+						bineHear.push_back({ testGardPos.x, testGardPos.y, testGardDir.x, testGardDir.y });
+					}
+
+					testGardPos.x += testGardDir.x;
+					testGardPos.y += testGardDir.y;
+				}
+			}
+
+			if (loopFound)
+			{
+				sum++;
+			}
+
+		}
+
+		std::cout << y << "\n";
+	}
+
+	/*
+	int2 gardDir = { 0, -1 };
+
+	std::vector<int2> obstacles;
+
+	std::vector<int2> FullBinHear;
+
+	bool LoopFound = false;
+
+	int iterations = 0;
+
+	while (gardPos.y < inpVec.size() - 1 && gardPos.y > 0 && gardPos.x < inpVec[0].size() - 1 && gardPos.x > 0)
+	{
+		if (inpVec[gardPos.y + gardDir.y][gardPos.x + gardDir.x] == BLOCK)
+		{
+			if (gardDir.y == -1 && gardDir.x == 0)
+			{
+				gardDir.y = 0;
+				gardDir.x = 1;
+			}
+			else if (gardDir.y == 0 && gardDir.x == 1)
+			{
+				gardDir.y = 1;
+				gardDir.x = 0;
+			}
+			else if (gardDir.y == 1 && gardDir.x == 0)
+			{
+				gardDir.y = 0;
+				gardDir.x = -1;
+			}
+			else if (gardDir.y == 0 && gardDir.x == -1)
+			{
+				gardDir.y = -1;
+				gardDir.x = 0;
+			}
+			else
+				throw;
+		}
+		else
+		{
+
+			if (!containes(obstacles, gardPos.x + gardDir.x, gardPos.y + gardDir.y))
+			{
+
+				std::vector<int4> bineHear;
+
+				std::vector<std::vector<char>> testInpVec(inpVec);
+
+				int2 testGardPos = gardPos;
+				int2 testGardDir = gardDir;
+
+				testInpVec[gardPos.y + gardDir.y][gardPos.x + gardDir.x] = BLOCK;
+
+				while (testGardPos.y < testInpVec.size() - 1 && testGardPos.y > 0 && testGardPos.x < testInpVec[0].size() - 1 && testGardPos.x > 0)
+				{
+					if (testInpVec[testGardPos.y + testGardDir.y][testGardPos.x + testGardDir.x] == BLOCK)
+					{
+						if (testGardDir.y == -1 && testGardDir.x == 0)
+						{
+							testGardDir.y = 0;
+							testGardDir.x = 1;
+						}
+						else if (testGardDir.y == 0 && testGardDir.x == 1)
+						{
+							testGardDir.y = 1;
+							testGardDir.x = 0;
+						}
+						else if (testGardDir.y == 1 && testGardDir.x == 0)
+						{
+							testGardDir.y = 0;
+							testGardDir.x = -1;
+						}
+						else if (testGardDir.y == 0 && testGardDir.x == -1)
+						{
+							testGardDir.y = -1;
+							testGardDir.x = 0;
+						}
+						else
+							throw;
+					}
+					else {
+
+						if (!containesi4(bineHear, testGardPos.x, testGardPos.y, testGardDir.x, testGardDir.y))
+						{
+							int4 tempInt4 = { testGardPos.x, testGardPos.y, testGardDir.x, testGardDir.y };
+							bineHear.push_back(tempInt4);
+						}
+						else
+						{
+							LoopFound = true;
+							break;
+						}
+
+					}
+				}
+
+				if (LoopFound)
+				{
+					int2 tempInt2 = { gardPos.x + gardDir.x, gardPos.y + gardDir.y };
+					obstacles.push_back(tempInt2);
+				}
+
+				LoopFound = false;
+
+			}
+
+			if (!containes(FullBinHear, gardPos.x, gardPos.y))
+			{
+				FullBinHear.push_back({ gardPos.x, gardPos.y });
+			}
+
+			gardPos.y += gardDir.y;
+			gardPos.x += gardDir.x;
+
+			std::cout << iterations << std::endl;
+			iterations++;
+		}
+	}
+
+	std::cout << FullBinHear.size() << "\n";
+
+	if (containes(obstacles, gardPosStart.x, gardPosStart.y))
+	{
+		std::cout << obstacles.size() - 1;
+	}
+	else
+	{
+		std::cout << obstacles.size();
+	}
+	*/
+
+	std::cout << sum;
+
+}
